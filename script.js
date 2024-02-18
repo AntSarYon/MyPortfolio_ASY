fd23ff300b8ce23ee6d70c81063056cb0a99f413
Antonio Sarmiento <antoniosaryon155@gmail.com>
2024-02-18 12:31:30 -0500

    NavBar and Hamb Menu Responsive

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu(){
+
+    //Obtenemos referencia a elementos de la página Web
+    //(en este caso, por medio de su clase)
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+
+    //Haremos que el elemento agregue o remueva la clase (estilo) Open
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+
+
+}
